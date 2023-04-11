ebf988e1cf85a8c0cbb42748b0b52ca86f098f8b
Christian <au668675@uni.au.dk>
2023-04-11 18:19:27 +0200

    Made the first 2 migrations

---
+++ Christian/EF_Core/Migrations/20230328142253_Initial.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Canteens",
+                columns: table => new
+                {
+                    CanteenId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Canteens", x => x.CanteenId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CprNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CprNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    MenuId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MenuType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CanteenId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.MenuId);
+                    table.ForeignKey(
+                        name: "FK_Menus_Canteens_CanteenId",
+                        column: x => x.CanteenId,
+                        principalTable: "Canteens",
+                        principalColumn: "CanteenId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    RatingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RatingValue = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CanteenId = table.Column<int>(type: "int", nullable: false),
+                    CprNumber = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => x.RatingId);
+                    table.ForeignKey(
+                        name: "FK_Ratings_Canteens_CanteenId",
+                        column: x => x.CanteenId,
+                        principalTable: "Canteens",
+                        principalColumn: "CanteenId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ratings_Customers_CprNumber",
+                        column: x => x.CprNumber,
+                        principalTable: "Customers",
+                        principalColumn: "CprNumber",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Meals",
+                columns: table => new
+                {
+                    MealId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MealType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    MenuId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meals", x => x.MealId);
+                    table.ForeignKey(
+                        name: "FK_Meals_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menus",
+                        principalColumn: "MenuId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    ReservationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cancelled = table.Column<bool>(type: "bit", nullable: false),
+                    CanteenId = table.Column<int>(type: "int", nullable: false),
+                    CprNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    MealId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.ReservationId);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Canteens_CanteenId",
+                        column: x => x.CanteenId,
+                        principalTable: "Canteens",
+                        principalColumn: "CanteenId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Customers_CprNumber",
+                        column: x => x.CprNumber,
+                        principalTable: "Customers",
+                        principalColumn: "CprNumber",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Meals_MealId",
+                        column: x => x.MealId,
+                        principalTable: "Meals",
+                        principalColumn: "MealId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meals_MenuId",
+                table: "Meals",
+                column: "MenuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Menus_CanteenId",
+                table: "Menus",
+                column: "CanteenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_CanteenId",
+                table: "Ratings",
+                column: "CanteenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_CprNumber",
+                table: "Ratings",
+                column: "CprNumber");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_CanteenId",
+                table: "Reservations",
+                column: "CanteenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_CprNumber",
+                table: "Reservations",
+                column: "CprNumber");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_MealId",
+                table: "Reservations",
+                column: "MealId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ratings");
+
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Meals");
+
+            migrationBuilder.DropTable(
+                name: "Menus");
+
+            migrationBuilder.DropTable(
+                name: "Canteens");
+        }
+    }
+}
---
+++ Christian/EF_Core/Models/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core.Models
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public bool Cancelled { get; set; }
+
+        public int CanteenId { get; set; }
+        public Canteen Canteen { get; set; }
+        [ForeignKey("Customer")]
+        public string AUID { get; set; }
+        public Customer Customer { get; set; }
+        public int MealId { get; set; }
+        public Meal Meal { get; set; }
+    }
+}
